c9724216ebf6e9a7f5552f29a503ae8f18f4b895
Ed Knutson <knutsoned+github@gmail.com>
2024-02-14 17:26:31 -0600

    merge @RaananW/babylonjs-webpack-es6 into BabylonJS Vite template

---
+++ src/createScene.ts
@@ -0,0 +1,21 @@
+import type { Engine } from "@babylonjs/core/Engines/engine";
+import type { Scene } from "@babylonjs/core/scene";
+
+// Change this import to check other scenes
+// any file in the scenes folder should do
+//import { DefaultSceneWithTexture } from "./scenes/defaultWithTexture";
+import { PhysicsSceneWithAmmo } from "./scenes/physicsWithAmmo";
+
+export interface CreateSceneClass {
+  createScene: (engine: Engine, canvas: HTMLCanvasElement) => Promise<Scene>;
+  preTasks?: Promise<unknown>[];
+}
+
+export interface CreateSceneModule {
+  default: CreateSceneClass;
+}
+
+export const getSceneModule = (): CreateSceneClass => {
+  //return new DefaultSceneWithTexture();
+  return new PhysicsSceneWithAmmo();
+};
